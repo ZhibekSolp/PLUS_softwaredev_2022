f6c689d4c8ab2b1bbd389f6efb0fb81ce47ecd7d
Sofía M. Delgado Balaguera <91962122+s1085496@users.noreply.github.com>
2022-03-21 12:05:43 +0100

    SofiasFile
    Hello! Please accept this commit

---
+++ SofiasFile.py
@@ -0,0 +1,6 @@
+# Hello Zhibek, it's Sofia here!
+# "Dream big and dare to fail" no idea who wrote this.
+# I don't like pasta.
+# Book Recommendation: The Unbearable Lightness of Being by Milan Kundera -> This a must once-in-a-lifetime.
+# German Word: Heute which means today haha.
+# Regards!
