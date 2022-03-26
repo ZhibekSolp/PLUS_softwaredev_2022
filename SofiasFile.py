415282c2b7311ccd04a29ba8df2c37c6262423d4
Sofía M. Delgado Balaguera <91962122+s1085496@users.noreply.github.com>
2022-03-26 19:09:48 +0100

    Update SofiasFile.py

--- SofiasFile.py
+++ SofiasFile.py
@@ -2,5 +2,5 @@
 # "Dream big and dare to fail" no idea who wrote this.
 # I don't like pasta.
 # Book Recommendation: The Unbearable Lightness of Being by Milan Kundera -> This a must once-in-a-lifetime.
-# German Word: Heute which means today haha.
+# German Word: "Heute" which means today haha.
 # Regards!
